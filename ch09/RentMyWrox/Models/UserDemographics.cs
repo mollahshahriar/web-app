14b7eb2708b92ddf7e4809836323a0c885c5bb61
mollahshahriar <mollahshahriar@outlook.com>
2020-04-08 21:38:28 -0400

    jj

---
+++ ch09/RentMyWrox/Models/UserDemographics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentMyWrox.Models
+{
+    public class UserDemographics
+    {
+        public UserDemographics()
+        {
+            Hobbies = new List<string>();
+        }
+
+        public DateTime Birthdate { get; set; }
+        public String Gender { get; set; }
+        public String MartialStatus { get; set; }
+        public DateTime DateMovedIntoArea { get; set; }
+        public bool OwnHome { get; set; }
+        public int TotalPeopleInHome { get; set; }
+        public List<String> Hobbies { get; set; }
+    }
+}
